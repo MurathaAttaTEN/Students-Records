dfd48a532cee7f75af84e3fc62fe21d30382c499
MurathaAttaTEN <attamuratha@gmail.com>
2022-02-13 14:56:44 +0300

    assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var student = "name"
+    println(student)
+    var age = 22
+    println(age)
+    var phoneNumber = 716457674
+    println(phoneNumber)
+    var weight: Float = 50.3F
+    println(weight)
+    val isKenyan: Boolean = true
+    val isNotKenyan: Boolean = false
+    println(isKenyan)
+    println(isNotKenyan)
+
+}
